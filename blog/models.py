4f743176029e8776ba9f59b58c594c0804cd2d18
Homam Almofty <h.almofty@gmail.com>
2021-06-01 18:57:01 +0100

    Job and Blog models up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/') #Inside the media folder created, anytime you upload an image it goes to the images folder
