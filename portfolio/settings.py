f9fd9002b4b0d6192e38da79cb3a1f629031e9b4
Homam Almofty <h.almofty@gmail.com>
2021-06-03 14:10:48 +0100

    Finished the blog and home page (polished)

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Djanopost272!',
+        'PASSWORD':'djano1234',
         'HOST': 'localhost',
         'PORT': '5432',
         
