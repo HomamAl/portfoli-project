cb39e2d9c23972b7e927c4f94bbbaf4bb0f40280
Homam Almofty <h.almofty@gmail.com>
2021-06-04 18:07:41 +0100

    added header

--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.fields import CharField
 
 class job(models.Model):
+    header = models.CharField(max_length=255, default='')
     image = models.ImageField(upload_to = 'images/') #Inside the media folder created, anytime you upload an image it goes to the images folder
     summary = CharField(max_length=500, default='') #To put cap to the summary boxes
 
