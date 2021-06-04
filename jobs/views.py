5ee3ee997fa4894a4f886bfb54bb907efd9f58a1
Homam Almofty <h.almofty@gmail.com>
2021-06-04 13:21:40 +0100

    undo the job settings

--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,6 @@
-from django.db import models
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import job
 
 def home(request):
     jobs = job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
-
-def detail(request, job_id):
-    detailjob = get_object_or_404(job, pk=job_id)    
-    return render(request, 'job/detail.html', {'job':detailjob})
