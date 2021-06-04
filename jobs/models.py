91b06618926e43f05e25b25d947ae11a2c91f96f
Homam Almofty <h.almofty@gmail.com>
2021-06-04 14:58:40 +0100

    Changing CV

--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.db.models.fields import CharField
 
 class job(models.Model):
+    title = models.CharField(max_length=255)
     image = models.ImageField(upload_to = 'images/') #Inside the media folder created, anytime you upload an image it goes to the images folder
     summary = CharField(max_length=200) #To put cap to the summary boxes
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.summary[:100]
