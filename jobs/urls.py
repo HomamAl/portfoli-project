4a145b291bd02a171e78a60a52567e56424160f2
Homam Almofty <h.almofty@gmail.com>
2021-06-04 15:04:18 +0100

    Back to original

--- jobs/urls.py
+++
@@ -1,8 +0,0 @@
-from typing import ValuesView
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('<int:jobs_id>/', views.detail, name='detail'),
-] 
--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import job
 
 def home(request):
     jobs = job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
-
-def detail(request, jobs_id):
-    detailjob = get_object_or_404(job, pk=jobs_id)    
-    return render(request, 'jobs/detail.html', {'job':detailjob})
