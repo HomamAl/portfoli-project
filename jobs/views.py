1a5a0fb31663e1279ff361b67c38e9155b9f7f0e
Homam Almofty <h.almofty@gmail.com>
2021-06-04 13:16:42 +0100

    testing the job dettail page

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.db import models
+from django.shortcuts import render, get_object_or_404
 from .models import job
 
 def home(request):
     jobs = job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+def detail(request, job_id):
+    detailjob = get_object_or_404(job, pk=job_id)    
+    return render(request, 'job/detail.html', {'job':detailjob})
