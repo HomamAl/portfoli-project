e6c88a06fbc6388c5b6f954517895d0894635cdf
Homam Almofty <h.almofty@gmail.com>
2021-06-07 16:36:59 +0100

    back to original

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,8 +6,6 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls')),
-    path('projects/', include('jobs.urls'))
-
+    path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
