21c0c3e5fb6f066aa084554dfc5eb00da4bbe0e9
Homam Almofty <h.almofty@gmail.com>
2021-06-07 19:21:35 +0100

    Finished first draft

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from .models import job
 
 def home(request):
     jobs = job.objects
-    return render(request, 'jobs/home.html', {'job':job})
-
-def details(request, jobs_id):
-    jobsdetail = get_object_or_404(job, pk=jobs_id)    
-    return render(request, 'jobs/details.html', {'work':jobsdetail}),
+    return render(request, 'jobs/home.html', {'jobs':jobs})
