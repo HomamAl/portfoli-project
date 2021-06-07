e780b2a51f7ffa6d831e4af43ed986ebc2cbd28e
Homam Almofty <h.almofty@gmail.com>
2021-06-07 14:28:48 +0100

    adding job details

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, render
 from .models import job
 
 def home(request):
     jobs = job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+def detail(request, job_id):
+    detailblog = get_object_or_404(job, pk=job_id)    
+    return render(request, 'blog/detail.html', {'blog':detailblog})
