34ddfc0e578902d89f1bd9765affbe4738681441
Homam Almofty <h.almofty@gmail.com>
2021-06-07 16:26:21 +0100

    adding projects

--- jobs/views.py
+++ jobs/views.py
@@ -3,8 +3,8 @@ from .models import job
 
 def home(request):
     jobs = job.objects
-    return render(request, 'jobs/home.html', {'job':jobs})
+    return render(request, 'jobs/home.html', {'jobs':jobs})
 
 def details(request, job_id):
     detailjobs = get_object_or_404(job, pk=job_id)    
-    return render(request, 'jobs/details.html', {'jobs':detailjobs})
+    return render(request, 'jobs/details.html', {'job':detailjobs})
